e4bea71fd577b5ad1a9fdd67e05bbf3a40e583b9
yw-fang <fyuewen@gmail.com>
2017-09-24 23:26:43 +0800

    code checked

--- machine-learning/McKinney-pythonbook2013/exercises/getmag-vasp/getmag-fang.py
+++ machine-learning/McKinney-pythonbook2013/exercises/getmag-vasp/getmag-fang.py
@@ -1,8 +1,36 @@
 import numpy as np
 
-file1 = open('OUTCAR')
-I = file1.readline()
-for i in I:
-    if 'TOTEN' in i:
-        print('OK')
-file1.close()
+path = './OUTCAR'
+path2 = 'mag-fang.out'
+
+f = open(path)
+
+linenumber = 0
+for line in  f:
+    linenumber = linenumber + 1
+    if 'magnetization (x)' in line:
+        #print(line)
+        linenumber_mag = linenumber
+f.close()
+
+#print(linenumber_mag)
+
+f = open(path)
+
+f2 = open(path2,'w')
+linenumber = 0
+
+for line in f:
+    linenumber = linenumber + 1
+    if linenumber >= linenumber_mag:
+        f2.write(line)
+#        with open('mag-fang.out', 'w') as handle:
+#            handle.writelines(line)
+        print(line)
+        if 'BZINTS' in line:
+            break
+    else:
+        continue
+
+f2.close()
+f.close()
