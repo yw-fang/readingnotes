e76364ca1c7bdbda74d6c750da3061e857a44c7c
yw-fang <fyuewen@gmail.com>
2018-02-16 01:14:47 +0900

    remove spider-1.py

--- matsci/project2018/thermal-togo-abel/source-db/spider-1.py
+++
@@ -1,8 +0,0 @@
-import urllib
-
-
-host = 'http://phonondb.mtl.kyoto-u.ac.jp/raw_data/'
-
-url = 'http://phonondb.mtl.kyoto-u.ac.jp/raw_data/'+'mp-25.tar.lzma'
-
-urllib.urlretrieve(url, 'mp-25.tar.lzma')
