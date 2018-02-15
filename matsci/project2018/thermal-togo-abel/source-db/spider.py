12f2d5533c3e7b21b7a8ef5b7c4ac327c956a2d2
yw-fang <fyuewen@gmail.com>
2018-02-16 01:31:08 +0900

    python spider for downloading the phonon databse has been finished

--- matsci/project2018/thermal-togo-abel/source-db/spider.py
+++ matsci/project2018/thermal-togo-abel/source-db/spider.py
@@ -1,3 +1,9 @@
+#Author: Yue-Wen FANG at Kyoto University
+#Contact: fyuewen@gmail.com
+######################################################
+#Purpose: This is a python crawler/spider to download#
+#the phonon database maintained by Togo-sensei       #
+#python 2.x should be used.
 import urllib2
 import re
 import urllib
@@ -12,13 +18,14 @@ links = re.findall('"((http)s?://.*.tar.lzma)"', html)
 
 pattern = re.compile(r'mp-\d+.tar.lzma')  #here, mp-\d+.tar.lzma is the pattern
 matchlist = pattern.findall(html)  #note in this matchlist, the comound ID is printed twice.
-new_matchlist = matchlist[::2]
+new_matchlist = matchlist[::2] #Here, I selected all the matched iterms, and discarded the duplicated ones
 print(new_matchlist[:10])
 print(type(new_matchlist))
 #print(' '.join(matchlist))  #through joinining, the quotations are removed. 
 
 for i in new_matchlist:
     file_url = 'http://phonondb.mtl.kyoto-u.ac.jp/raw_data/' + str(i)
+    urllib.urlretrieve(file_url, i) #download files with the name of i.
     print(file_url)
-
-#urllib.urlretrieve(url, 'mp-25.tar.lzma')
+#usage of urllib.urlretrieve
+#urllib.urlretrieve(url, filename) #here, filename should be a string
