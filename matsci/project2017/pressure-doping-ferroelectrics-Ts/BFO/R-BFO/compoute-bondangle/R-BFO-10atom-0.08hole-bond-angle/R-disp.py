9113705ba07bbd8307b68685e38a7866d52bb1fb
yw-fang <fyuewen@gmail.com>
2018-04-04 14:32:03 +0900

    disp, bond-angle of 0.08hole doped R-BFO 10atoms

---
+++ matsci/project2017/pressure-doping-ferroelectrics-Ts/BFO/R-BFO/compoute-bondangle/R-BFO-10atom-0.08hole-bond-angle/R-disp.py
@@ -0,0 +1,35 @@
+from pymatgen.io.vasp import Poscar
+# from pymatgen import *
+from numpy import pi
+from numpy import arccos, dot  # pi
+from numpy.linalg import norm
+import numpy as np
+
+yourfile = input('Choose a file: ')
+p = Poscar.from_file(yourfile)
+
+
+Fe1 = p.structure.frac_coords[2]
+print(Fe1)
+O1_Fe = p.structure.frac_coords[9]
+print(O1_Fe)
+O2_Fe = p.structure.frac_coords[8]
+print(O2_Fe)
+a = Fe1 - O1_Fe
+b = Fe1 - O2_Fe
+
+
+theta_rad = arccos(dot(a,b)/(norm(a)*norm(b)))  #in radians
+#print(theta_rad)
+theta_deg = theta_rad*180./pi
+print(theta_deg)
+
+lattice_vector_R = p.structure.lattice.matrix
+print(lattice_vector_R)
+lattice_c_hexagonal = lattice_vector_R[0][2]*3
+print(lattice_c_hexagonal)
+
+high_symmetry= np.array([0.5, 0.5, 0.5])
+relative_disp_frac = Fe1-high_symmetry[0]
+relative_disp_cart = relative_disp_frac[1]*lattice_c_hexagonal
+print(relative_disp_cart,' angstrom')
