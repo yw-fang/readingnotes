6467837418d8d68def227806418e47989a2a821d
yw-fang <fyuewen@gmail.com>
2018-04-17 23:03:04 +0900

    structure of sqrt2-sqrt2-2-1Ca, 20atoms/supercell

---
+++ matsci/project2017/pressure-doping-ferroelectrics-Ts/BFO/T-BFO-sqrt2-sqrt2-2-1Ca-POSCAR/bond-angle/loop-bondangle.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for((i=0;i<=120;i=i+10));
+#for((i=65;i<=75;i=i+5));
+#for((i=200;i<=800;i=i+100));
+do
+cp ../$i/CONTCAR ./POSCAR-$i.vasp
+done
+
+
+#for((i=0;i<=120;i=i+20));
+#for((i=200;i<=800;i=i+100));
+#do
+#cp ../$i/scf/POSCAR ./POSCAR-$i.vasp
+#done
