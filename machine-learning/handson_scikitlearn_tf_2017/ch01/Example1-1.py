6653e2504741be80de0f5f62a2342190ede8ce54
yw-fang <fyuewen@gmail.com>
2018-06-01 11:31:53 +0900

    revise

--- machine-learning/handson_scikitlearn_tf_2017/ch01/Example1-1.py
+++ machine-learning/handson_scikitlearn_tf_2017/ch01/Example1-1.py
@@ -1,8 +1,9 @@
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-import pands as np
+import pandas as pd
 import sklearn
 
-#load the data
-oecd_bli = pd.read_csv('')
+# load the data
+x = pd.read_csv("oecd_bli_2015.csv", error_bad_lines=False)
+print(x.head(10))
