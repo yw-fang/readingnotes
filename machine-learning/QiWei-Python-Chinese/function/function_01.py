6ef00f838acf3a1505587bb9bc1e83760b620e9b
yw-fang <fyuewen@gmail.com>
2018-12-25 00:30:26 +0900

    add author and licence information.

--- machine-learning/QiWei-Python-Chinese/function/function_01.py
+++ machine-learning/QiWei-Python-Chinese/function/function_01.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+__author__ = 'Yue-Wen FANG'
+__maintainer__ = "Yue-Wen FANG"
+__email__ = 'fyuewen@gmail.com'
+__license__ = 'Apache License 2.0'
+__creation_date__= 'Dec. 25, 2018'
+
 """
 This example shows that documentation for the functions
 you defined is important for others who read your codes.
--- machine-learning/QiWei-Python-Chinese/function/function_02.py
+++ machine-learning/QiWei-Python-Chinese/function/function_02.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+__author__ = 'Yue-Wen FANG'
+__maintainer__ = "Yue-Wen FANG"
+__email__ = 'fyuewen@gmail.com'
+__license__ = 'Apache License 2.0'
+__creation_date__= 'Dec. 25, 2018'
+
 """
 This example shows the functionality
 of positional arguments and keyword ONLY arguments.
