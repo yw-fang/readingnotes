74bddf4214e8edf8262ffabfacf825200c8164fb
yw-fang <fyuewen@gmail.com>
2019-03-20 15:04:09 +0900

    tip of variables used in python

---
+++ machine-learning/Matthes-crash-course/chapt02/hello_world.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Yue-Wen FANG'
+__maintainer__ = "Yue-Wen FANG"
+__email__ = 'fyuewen@gmail.com'
+__license__ = 'Apache License 2.0'
+__creation_date__= 'Mar. 20, 2019'
+
+message = "Hello Python world!"
+print(message)
+
+tip_1 = "cautious to use letter l and capital letter O because l is like 1 and O is like 0"
+print(tip_1)
+
+"""
+在些python程序时候，如果遇到程序中的变量名相关错误，那么这种错误通常来自于2个原因：
+1）undefined 
+2）typo error of the variable
+"""
