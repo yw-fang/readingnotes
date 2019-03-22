026a81a29351075c13c839b89114bd3120adc12e
yw-fang <fyuewen@gmail.com>
2019-03-22 11:37:22 +0900

    whitespace print

---
+++ machine-learning/Matthes-crash-course/chapt02/print_names_03.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Yue-Wen FANG'
+__maintainer__ = "Yue-Wen FANG"
+__email__ = 'fyuewen@gmail.com'
+__license__ = 'Apache License 2.0'
+__creation_date__= 'Mar. 20, 2019'
+
+"""
+Exercise
+"""
+
+name = "Niels Bohr  "
+quote = "Prediction is very difficult, especially about the future."
+a = '  Quantum  '
+a = a.strip()
+
+name_strip = name.rstrip()
+print(name_strip, "is a scientist")
+print(name, "is a scientist")
+
+print("\t",quote,"\n", "\t This was said by Bohr")
+
+print(a)
